24b9c62ba9b7a8d855eb7b547f63a8bc926bca39
YaminaBoghossian <yamina.boghossian@gmail.com>
2017-08-22 11:33:43 +0200

    test  js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 //va heberger notre librairie
+
+"use strict";
+
+function fetch(url) {
+    return new Promise(function(ok, ko) {
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.onload = function() {
+            if (xhr.status == 200) {
+                ok(xhr.response);
+            } else {
+                ko(xhr.status)
+            }
+        };
+        xhr.onerror = function(error) {
+            ko(error);
+        }
+        xhr.send();
+    });
+}
+module.exports = fetch;
+fetch("https://dog.ceo/api/breeds/list/all").then(function(response) {
+    console.log(response)
+}).catch(function(error) {
+    console.error(error);
+});
--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 //va tester notre librairie
+
+
+const fetch = require('.index.js');
+fetch('https://dog.ceo/api/breeds/list/all')
+    .then(function(answer) {
+        console.log(answer);
+    }).catch(function(err) {
+        console.error(err);
+    });
